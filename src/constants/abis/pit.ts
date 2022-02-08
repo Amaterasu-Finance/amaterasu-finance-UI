9a2038756da41601302c26bc7ac7d6f84070966d
FoxTheFarmer <FoxTheFarmer@cryptina.info>
2022-02-07 19:38:41 -0700

    fix pit abi

--- src/constants/abis/pit.ts
+++ src/constants/abis/pit.ts
@@ -1,5 +1,5 @@
 import { Interface } from '@ethersproject/abi'
-import { abi as PIT_ABI } from '@venomswap/contracts/build/Pit.json'
+import PIT_ABI from './pit.json'
 
 const PIT_INTERFACE = new Interface(PIT_ABI)
 
